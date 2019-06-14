0d0749aa4a2c003807d292d5f5e82cf0bc76639f
threegeese <19970913qjh@gmail.com>
2019-06-14 18:33:01 +0800

    first commit

---
+++ jrg-01/main.js
@@ -0,0 +1,22 @@
+let number = 0;
+
+let onClickButton = ()=> {
+    number += 1;
+    render();
+}
+
+let onClickButton2 = ()=> {
+    number -= 1;
+    render();
+}
+
+render();
+
+
+function render() {
+    let span = React.createElement('span', { className: "red" }, number);
+    let button = React.createElement('button', { onClick: onClickButton }, '+')
+    let button2 = React.createElement('button', { onClick: onClickButton2 }, '-')
+    let div = React.createElement('div', { className: 'parent' }, span, button, button2)
+    ReactDOM.render(div, document.querySelector("#root"));
+}
