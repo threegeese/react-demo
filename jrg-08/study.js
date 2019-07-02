45dcb7a07aa7464a6e2121bf193238b9bcf1c6e1
threegeese <19970913qjh@gmail.com>
2019-07-02 17:35:45 +0800

    react router

---
+++ jrg-08/study.js
@@ -0,0 +1,35 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+import "./index.css"
+
+function HomePage() {
+  return <div class="showPage">首页</div>
+}
+
+function Login() {
+  return <div class="showPage">登录</div>
+}
+
+function SignUp() {
+  return <div class="showPage">注册</div>
+}
+
+
+function App() {
+  return (
+    <Router>
+      <div className="btns">
+        <Link to="/welcome"><button>首页</button></Link>
+        <Link to="/login"><button>登录</button></Link>
+        <Link to="/signup"><button>注册</button></Link>
+      </div>
+      <Route path="/welcome" component={HomePage} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
