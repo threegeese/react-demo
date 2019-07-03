a16dbf3e7eec7c895af24a8a6e162ab8b6590798
threegeese <19970913qjh@gmail.com>
2019-07-03 09:05:09 +0800

    div life cycle

---
+++ jrg-09/vanilla.js
@@ -0,0 +1,30 @@
+/**
+ * 一个 div 的生老病死
+ */
+let app = document.querySelector("#app")
+
+// create
+let div = document.createElement("div")
+
+let state = 0
+div.innerHTML = `
+  <p>${state}</p>
+  <button>+1</button>
+  <button>die</button>
+`
+
+// mount
+app.appendChild(div)
+
+div.querySelector("button").onclick = ()=> {
+  state += 1
+  div.querySelector("p").innerText = `${state}`   // update
+}
+
+// destroy
+div.querySelectorAll("button")[1].onclick = ()=> {
+  div.querySelector("button").onclick = null
+  div.querySelectorAll("button").onclick[1] = null
+  div.remove()
+  div = null
+}
