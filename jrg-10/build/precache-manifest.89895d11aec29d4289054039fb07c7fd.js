84207d2f728cf1fa079c6fec3f83db7bbf83c1f7
threegeese <19970913qjh@gmail.com>
2019-07-04 11:47:31 +0800

    add preview

---
+++ jrg-10/build/precache-manifest.89895d11aec29d4289054039fb07c7fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b47ab24a0545f00a84a4af7f3248c69",
+    "url": "/react-demo/jrg-10/build/index.html"
+  },
+  {
+    "revision": "413efb2a6b25f010d2ee",
+    "url": "/react-demo/jrg-10/build/static/css/main.ba990c96.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-demo/jrg-10/build/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "413efb2a6b25f010d2ee",
+    "url": "/react-demo/jrg-10/build/static/js/main.edc9289b.chunk.js"
+  },
+  {
+    "revision": "73df9b5cb506bc3ad983",
+    "url": "/react-demo/jrg-10/build/static/js/runtime~main.8f656766.js"
+  }
+]);
