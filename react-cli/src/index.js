496f09ecda635dcdbec815cb573b1dbbec462382
threegeese <19970913qjh@gmail.com>
2019-09-07 20:51:37 +0800

    react document demo

--- react-cli/src/index.js
+++ react-cli/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom'
 
 // import App from './app.js'
 import Clock from './clock.js'
+import TemperatureCalculator from './components/TemperatureCalculator.js'
+
 
 import './index.css'
 
 // ReactDOM.render(<App />, document.getElementById('root'))
 ReactDOM.render(<Clock />, document.getElementById('root'))
+ReactDOM.render(<TemperatureCalculator />, document.getElementById('root'))
